78fcf762bc7c55417a6dfccc4483ba591710579c
LiLi <2206915473@qq.com>
2022-05-16 20:18:03 +0800

    First Commit

---
+++ git学习/git学习/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  git学习
+//
+//  Created by 李立 on 2022/5/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
